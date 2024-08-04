29732d0a577a13baafbd6a623c6d95c06b68e94f
jvmaiaa <joaovictormaia482@gmail.com>
2024-08-04 02:02:07 -0300

    Estrutura inicial do Projeto

---
+++ src/main/java/com/ajuda/ajuda_ai/AjudaAiApplication.java
@@ -0,0 +1,13 @@
+package com.ajuda.ajuda_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajuda/ajuda_ai/AjudaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajuda.ajuda_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
